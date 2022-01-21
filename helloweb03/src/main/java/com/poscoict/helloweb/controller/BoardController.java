a1292126f16d527c4aac27b105b698650c519070
BIT <BIT@DESKTOP-7FO0C42>
2022-01-21 09:51:40 +0900

    spring practice#3

---
+++ helloweb03/src/main/java/com/poscoict/helloweb/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.poscoict.helloweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * 
+ * @RequestMapping: 메소드 단독 매핑
+ *
+ */
+@Controller
+public class BoardController {
+
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController.write() ";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view")
+	public String view(Long no) {		
+		return "BoardController.view(" + no + ")";
+	}
+	
+}
---
+++ helloweb03/src/main/java/com/poscoict/helloweb/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.poscoict.helloweb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @RequestMapping: 클래스(타입) + 메소드(핸들러)
+ *
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
+	
+}

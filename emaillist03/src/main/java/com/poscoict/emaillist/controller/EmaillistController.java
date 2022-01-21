1b6a6cb5cd0df106c409a09ce00fab123e040b90
BIT <BIT@DESKTOP-7FO0C42>
2022-01-21 11:57:07 +0900

    emaillist03 #3

--- emaillist03/src/main/java/com/poscoict/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/poscoict/emaillist/controller/EmaillistController.java
@@ -1,15 +1,26 @@
 package com.poscoict.emaillist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.poscoict.emaillist.repository.EmaillistRepository;
+import com.poscoict.emaillist.vo.EmaillistVo;
+
 @Controller
 public class EmaillistController {
+	@Autowired
+	private EmaillistRepository emaillistRepository;
 	
 	@ResponseBody
 	@RequestMapping("")
 	public String index() {
+		System.out.println(emaillistRepository);
+		//List<EmaillistVo> list = emaillistRepository.findAll();
+		
 		return "emaillist";
 	}
 }

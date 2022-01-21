9c2b33c02f6c1f411161af0250eb734dca842c26
BIT <BIT@DESKTOP-7FO0C42>
2022-01-21 11:35:09 +0900

    emaillist03 #2

--- emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
+++ emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
@@ -8,8 +8,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.poscoict.emaillist.vo.EmaillistVo;
 
+@Repository
 public class EmaillistRepository {
 	public List<EmaillistVo> findAll() {
 		List<EmaillistVo> result = new ArrayList<>();

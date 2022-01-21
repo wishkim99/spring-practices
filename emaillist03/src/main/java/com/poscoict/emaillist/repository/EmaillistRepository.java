382d93394941f92f3314a954cbf9b9f34637b8d3
BIT <BIT@DESKTOP-7FO0C42>
2022-01-21 11:24:38 +0900

    emaillist03 #1

---
+++ emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
@@ -0,0 +1,129 @@
+package com.poscoict.emaillist.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.poscoict.emaillist.vo.EmaillistVo;
+
+public class EmaillistRepository {
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();			
+		
+			//3. SQL 준비
+			String sql = "select no, first_name, last_name, email from emaillist order by no desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. 바인딩(binding)
+			
+			//5. SQL 실행
+			rs = pstmt.executeQuery();
+		
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String email = rs.getString(4);
+				
+				EmaillistVo vo = new EmaillistVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+				
+				result.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.print("error : " + e);
+		} finally {
+			// 자원 정리
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public boolean insert(EmaillistVo vo) {
+		boolean result = false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();			
+		
+			//3. SQL 준비
+			String sql = "insert into emaillist values(null, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			//4. 바인딩(binding)
+			pstmt.setString(1, vo.getFirstName());
+			pstmt.setString(2, vo.getLastName());
+			pstmt.setString(3, vo.getEmail());
+			
+			//5. SQL 실행
+			result = pstmt.executeUpdate() == 1;
+		
+		} catch (SQLException e) {
+			System.out.print("error : " + e);
+		} finally {
+			// 자원 정리
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		
+		try {
+			//1. JDBC 드라이버 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//2. 연결하기
+			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch(ClassNotFoundException e) {
+			System.out.print("드라이버 로딩 실패 :" + e);
+		} 
+		
+		return conn;
+	}
+}

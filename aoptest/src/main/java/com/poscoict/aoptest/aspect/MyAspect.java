a2ac0c84e121bb3c64700c35fdd86dacceeb19dd
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-01-27 11:57:34 +0900

    aop #1

---
+++ aoptest/src/main/java/com/poscoict/aoptest/aspect/MyAspect.java
@@ -0,0 +1,15 @@
+package com.poscoict.aoptest.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+	
+	@Before("execution(public com.poscoict.aoptest.vo.ProductVo com.poscoict.aoptest.service.ProductService.find(String))")
+	public void beforeAdvice() {
+		System.out.println("---Before Advice---");
+	}
+}
---
+++ aoptest/src/main/java/com/poscoict/aoptest/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.poscoict.aoptest.service;
+
+import org.springframework.stereotype.Service;
+
+import com.poscoict.aoptest.vo.ProductVo;
+
+@Service
+public class ProductService {
+	public ProductVo find(String name) {
+		System.out.println("[ProductService] finding....");
+		return new ProductVo(name);
+	}
+
+}

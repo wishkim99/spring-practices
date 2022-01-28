e44417e011d01759d2a83e7b1613e9bd3edba88e
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-01-28 16:36:14 +0900

    tomcat 플러그인 설정

--- emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
+++ emaillist03/src/main/java/com/poscoict/emaillist/repository/EmaillistRepository.java
@@ -118,10 +118,10 @@ public class EmaillistRepository {
 		
 		try {
 			//1. JDBC 드라이버 로딩
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			String url = "jdbc:mysql://192.168.0.51:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch(ClassNotFoundException e) {
 			System.out.print("드라이버 로딩 실패 :" + e);
--- guestbook03/src/main/java/com/poscoict/guestbook/repository/GuestbookRepository.java
+++ guestbook03/src/main/java/com/poscoict/guestbook/repository/GuestbookRepository.java
@@ -149,8 +149,11 @@ public class GuestbookRepository {
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
+			//1. JDBC 드라이버 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			//2. 연결하기
+			String url = "jdbc:mysql://192.168.0.51:3307/webdb?characterEncoding=UTF-8&serverTimezone=UTC";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
